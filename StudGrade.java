1042412b6b4c4090c969fbbb12939d079f81c754
saammmyyyy <100707863+saammmyyyy@users.noreply.github.com>
2024-07-12 18:16:05 +0530

    Add files via upload

---
+++ StudGrade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class StudGrade{
+    public static void main(String[] args) 
+    {
+
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the number of Grades:");
+    int numGrades = scanner.nextInt();
+    int[] grades = new int[numGrades];
+    for(int i=0;i<numGrades;i++)
+    {
+        System.out.println("Enter the Grade: " + (i+1) + ": ");
+        grades[i]=scanner.nextInt();
+}
+scanner.close();
+int total=0;
+for(int i=0;i<numGrades;i++)
+{
+    total+=grades[i];
+
+}
+double average = (double)total/numGrades;
+System.out.println("The average is: " + average);
+
+
+    }
+}
